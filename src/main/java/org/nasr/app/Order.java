cb59e1ce813903fa0a3ee5adf4caa9451c4caa6b
Al-fredo07 <abdelrahman.nasreldin.student@pua.edu.eg>
2020-04-06 22:22:06 +0200

    calculte total price regrading product type

--- src/main/java/org/nasr/app/Order.java
+++ src/main/java/org/nasr/app/Order.java
@@ -2,39 +2,57 @@ package org.nasr.app;
 
 public class Order {
     private String name ;
-    private String cardType;
-    private int discount;
-    private int price ;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCardType() {
-        return cardType;
-    }
-
-    public int getDiscount() {
-        return discount;
-    }
-
-    public int getPrice() {
-        return price;
-    }
+    private String productType;
+    private double discount;
+    private double price ;
+    private double tax;
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setCardType(String cardType) {
-        this.cardType = cardType;
+    public void setProductType(String productType) {
+        this.productType = productType;
     }
 
-    public void setDiscount(int discount) {
+
+    public void setDiscount(double discount) {
         this.discount = discount;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void totalPrice()
+    {
+
+        this.price = (1 + this.tax - this.discount ) * this.price;
+        System.out.println(this.price);
     }
 }
