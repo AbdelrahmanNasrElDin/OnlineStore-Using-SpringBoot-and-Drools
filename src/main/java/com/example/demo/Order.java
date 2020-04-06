3755ec3e2837049036e4330dc7772a2cf030560f
Al-fredo07 <abdelrahman.nasreldin.student@pua.edu.eg>
2020-04-06 04:41:11 +0200

    integrate spring boot with Drools

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Order {
+    private String name ;
+    private String cardType;
+    private int discount;
+    private int price ;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
